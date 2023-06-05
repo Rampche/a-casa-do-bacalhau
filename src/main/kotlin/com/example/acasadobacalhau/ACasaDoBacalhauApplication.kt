8dab51cab3adf4e3f9394bcdfc67539d277d80f2
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-06-05 15:09:59 +0100

    Initial commit. Project created.

---
+++ src/main/kotlin/com/example/acasadobacalhau/ACasaDoBacalhauApplication.kt
@@ -0,0 +1,11 @@
+package com.example.acasadobacalhau
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ACasaDoBacalhauApplication
+
+fun main(args: Array<String>) {
+	runApplication<ACasaDoBacalhauApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/acasadobacalhau/ACasaDoBacalhauApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.acasadobacalhau
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ACasaDoBacalhauApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
