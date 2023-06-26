876b62c7441fdecc967f5f30882c4530a0114252
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-06-26 13:54:30 -0300

    Updated items repository.

--- src/main/kotlin/com/example/acasadobacalhau/repository/ItemsRepository.kt
+++ src/main/kotlin/com/example/acasadobacalhau/repository/ItemsRepository.kt
@@ -5,5 +5,5 @@ import com.example.acasadobacalhau.models.ItemsModel
 import org.springframework.data.repository.CrudRepository
 
 interface ItemsRepository:CrudRepository<ItemsModel, Int> {
-    
+
 }
