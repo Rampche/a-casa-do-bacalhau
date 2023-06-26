220058b8c6a29684da17f4b56abaac3b506264b2
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-06-26 13:52:31 -0300

    Created and updated items model

---
+++ src/main/kotlin/com/example/acasadobacalhau/models/ItemsModel.kt
@@ -0,0 +1,22 @@
+package com.example.acasadobacalhau.models
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.*
+import java.math.BigDecimal
+
+@Entity(name="items")
+data class ItemsModel(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="item_id")
+    var id: Int? = null,
+    @Column
+    var name: String,
+    @Column
+    var price: BigDecimal,
+    @Column
+    var description: String
+)
