17a060124a1c4ff44c8690bf966c2dbdca3578a7
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-06-27 09:13:44 -0300

    Updated with employees functions.

--- src/main/kotlin/com/example/acasadobacalhau/extensions/ConverterExtensionFunction.kt
+++ src/main/kotlin/com/example/acasadobacalhau/extensions/ConverterExtensionFunction.kt
@@ -1,9 +1,11 @@
 package com.example.acasadobacalhau.extensions
 
-import com.example.acasadobacalhau.controller.requests.PatchCustomerRequest
-import com.example.acasadobacalhau.controller.requests.PostCustomerRequest
-import com.example.acasadobacalhau.controller.requests.PutCustomerRequest
+import com.example.acasadobacalhau.controller.requests.customer.PostCustomerRequest
+import com.example.acasadobacalhau.controller.requests.customer.PutCustomerRequest
+import com.example.acasadobacalhau.controller.requests.employee.PostEmployeeRequest
+import com.example.acasadobacalhau.controller.requests.employee.PutEmployeeRequest
 import com.example.acasadobacalhau.models.CustomerModel
+import com.example.acasadobacalhau.models.EmployeeModel
 
 fun PostCustomerRequest.toCustomerModel(): CustomerModel {
 return CustomerModel(name = this.name, email=this.email, password = this.password)
@@ -11,4 +13,13 @@ return CustomerModel(name = this.name, email=this.email, password = this.passwor
 
 fun PutCustomerRequest.toCustomerModel(previousValue: CustomerModel ):CustomerModel {
     return CustomerModel(id = previousValue.id, name = this.name ?: previousValue.name, email=this.email ?: previousValue.email, password = this.password ?: previousValue.password)
+}
+
+
+fun PostEmployeeRequest.toEmployeeModel():EmployeeModel{
+    return EmployeeModel(name = this.name,  email=this.email, password = this.password, role = this.role)
+}
+
+fun PutEmployeeRequest.toEmployeeModel(previousValue: EmployeeModel ):EmployeeModel {
+    return EmployeeModel(id = previousValue.id, name = this.name ?: previousValue.name, email=this.email ?: previousValue.email, password = this.password ?: previousValue.password, role = this.role ?: previousValue.role)
 }
