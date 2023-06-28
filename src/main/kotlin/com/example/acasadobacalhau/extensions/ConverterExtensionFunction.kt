ab639392eae80dd033aa8ef76f3a71bad2073a5a
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-06-28 19:02:49 -0300

    Addition of functions for reservations.

--- src/main/kotlin/com/example/acasadobacalhau/extensions/ConverterExtensionFunction.kt
+++ src/main/kotlin/com/example/acasadobacalhau/extensions/ConverterExtensionFunction.kt
@@ -4,8 +4,14 @@ import com.example.acasadobacalhau.controller.requests.customer.PostCustomerRequ
 import com.example.acasadobacalhau.controller.requests.customer.PutCustomerRequest
 import com.example.acasadobacalhau.controller.requests.employee.PostEmployeeRequest
 import com.example.acasadobacalhau.controller.requests.employee.PutEmployeeRequest
+import com.example.acasadobacalhau.controller.requests.reservation.PostReservationRequest
+import com.example.acasadobacalhau.controller.requests.reservation.PutReservationRequest
+import com.example.acasadobacalhau.controller.requests.tables.PostTableRequest
+import com.example.acasadobacalhau.controller.requests.tables.PutTableRequest
 import com.example.acasadobacalhau.models.CustomerModel
 import com.example.acasadobacalhau.models.EmployeeModel
+import com.example.acasadobacalhau.models.ReservationModel
+import com.example.acasadobacalhau.models.TableModel
 
 fun PostCustomerRequest.toCustomerModel(): CustomerModel {
 return CustomerModel(name = this.name, email=this.email, password = this.password)
@@ -22,4 +28,20 @@ fun PostEmployeeRequest.toEmployeeModel():EmployeeModel{
 
 fun PutEmployeeRequest.toEmployeeModel(previousValue: EmployeeModel ):EmployeeModel {
     return EmployeeModel(id = previousValue.id, name = this.name ?: previousValue.name, email=this.email ?: previousValue.email, password = this.password ?: previousValue.password, role = this.role ?: previousValue.role)
+}
+
+fun PostTableRequest.toTableModel(): TableModel{
+    return TableModel(id = this.id, number = this.number, capacity = this.capacity, status = this.status)
+}
+
+fun PutTableRequest.toTableModel(previousValue: TableModel): TableModel{
+    return TableModel(id = previousValue.id, number = previousValue.number, capacity = previousValue.capacity, status = previousValue.status)
+}
+
+fun PostReservationRequest.toReservationModel(customer: CustomerModel, table: TableModel):ReservationModel{
+    return ReservationModel(date = this.date, time = this.time, partySize = this.partySize, customer = customer, table = table)
+}
+
+fun PutReservationRequest.toReservationModel(previousValue: ReservationModel):ReservationModel{
+    return ReservationModel(id = previousValue.id, date = previousValue.date, time = previousValue.time, partySize = previousValue.partySize, customer = previousValue.customer, table = previousValue.table)
 }
