c60e8166be6c84f47f8f8878716a75bb41031890
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-06-28 18:32:42 -0300

    Text adjustment.

--- src/main/kotlin/com/example/acasadobacalhau/controller/requests/employee/PostEmployeeRequest.kt
+++ src/main/kotlin/com/example/acasadobacalhau/controller/requests/employee/PostEmployeeRequest.kt
@@ -3,7 +3,6 @@ package com.example.acasadobacalhau.controller.requests.employee
 import com.example.acasadobacalhau.enums.RoleType
 
 data class PostEmployeeRequest (
-
     val name: String,
     val email: String,
     val password: String,
