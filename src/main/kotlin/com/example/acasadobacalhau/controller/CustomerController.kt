123b4796c7c2b5a41845adbccd5918718cdc607d
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-06-28 18:59:21 -0300

    Idententation adjustments.

--- src/main/kotlin/com/example/acasadobacalhau/controller/CustomerController.kt
+++ src/main/kotlin/com/example/acasadobacalhau/controller/CustomerController.kt
@@ -2,9 +2,12 @@ package com.example.acasadobacalhau.controller
 
 import com.example.acasadobacalhau.controller.requests.customer.PostCustomerRequest
 import com.example.acasadobacalhau.controller.requests.customer.PutCustomerRequest
+import com.example.acasadobacalhau.controller.requests.reservation.PostReservationRequest
 import com.example.acasadobacalhau.extensions.toCustomerModel
+import com.example.acasadobacalhau.extensions.toReservationModel
 import com.example.acasadobacalhau.models.CustomerModel
 import com.example.acasadobacalhau.services.CustomerService
+import com.example.acasadobacalhau.services.ReservationService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -40,7 +43,6 @@ class CustomerController(val customerService: CustomerService) {
     fun findActives():List<CustomerModel>{
         return customerService.findActives()
     }
-
 
     @GetMapping("/{id}")
     fun findCustomerById(@PathVariable id:Int):CustomerModel{
