4b8979bc6d857934f15267c0bbf8c17f2c618061
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-07-03 23:07:25 -0300

    Added command to hide null objects from the json result.

---
+++ src/main/kotlin/com/example/acasadobacalhau/controller/response/CustomerResponse.kt
@@ -0,0 +1,9 @@
+package com.example.acasadobacalhau.controller.response
+
+import com.example.acasadobacalhau.models.ReservationModel
+
+data class CustomerResponse (
+    val id: Int,
+    val name: String,
+    val email: String,
+)
