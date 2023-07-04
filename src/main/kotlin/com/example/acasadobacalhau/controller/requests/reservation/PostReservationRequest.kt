c96acfd5c786f00470c779aaa2076d8fee327b68
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-07-03 22:56:20 -0300

    Added Id.

--- src/main/kotlin/com/example/acasadobacalhau/controller/requests/reservation/PostReservationRequest.kt
+++ src/main/kotlin/com/example/acasadobacalhau/controller/requests/reservation/PostReservationRequest.kt
@@ -7,6 +7,7 @@ import java.time.LocalDate
 import java.time.LocalTime
 
 data class PostReservationRequest (
+    val id: Int?,
     val date: LocalDate,
     val time: LocalTime,
     val partySize: Int,
