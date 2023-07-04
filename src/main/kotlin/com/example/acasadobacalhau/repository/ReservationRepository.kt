82c579c145828050917438f342893a0a4631343f
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-07-03 22:58:32 -0300

    File renamed.

--- src/main/kotlin/com/example/acasadobacalhau/repository/ReservationRepository.kt
+++ src/main/kotlin/com/example/acasadobacalhau/repository/ReservationRepository.kt
@@ -3,5 +3,5 @@ package com.example.acasadobacalhau.repository
 import com.example.acasadobacalhau.models.ReservationModel
 import org.springframework.data.repository.CrudRepository
 
-interface ReservationRespository:CrudRepository <ReservationModel, Int> {
+interface ReservationRepository:CrudRepository <ReservationModel, Int> {
 }
