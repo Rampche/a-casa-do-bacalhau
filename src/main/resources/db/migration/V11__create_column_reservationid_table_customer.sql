786bbfb80ab6c8cb0eae817e99d49d13ec5dc8cb
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-07-03 23:05:23 -0300

    Updates to the schemas.

--- src/main/resources/db/migration/V11__create_column_reservationid_table_customer.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE customer
-ADD COLUMN reservation_id int,
-ADD FOREIGN KEY (reservation_id) REFERENCES reservation(reservation_id)
--- src/main/resources/db/migration/V6__create_orders_table.sql
+++ src/main/resources/db/migration/V6__create_orders_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE orders(
 orders_id int auto_increment primary key,
-reservationId int,
-FOREIGN KEY (reservationId) REFERENCES reservation(reservation_id),
-employeeId int,
-FOREIGN KEY (employeeId) REFERENCES employee(employee_id),
+reservation_id int,
+FOREIGN KEY (reservation_id) REFERENCES reservation(reservation_id),
+employee_id int,
+FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
 schedule time(0),
-customerId int,
-FOREIGN KEY (customerId) REFERENCES customer(customer_id)
+customer_id int,
+FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
